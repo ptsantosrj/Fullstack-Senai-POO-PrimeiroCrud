ab838e647d0bea8f5627f386e13a4cf91a068df3
Arthur Giangiarulo <111184548+arthurGiangiarulo@users.noreply.github.com>
2024-07-18 18:48:14 -0300

    Exception detalhado

--- senai/src/main/java/com/api/senai/SenaiApplication.java
+++ senai/src/main/java/com/api/senai/SenaiApplication.java
@@ -1,5 +1,8 @@
 package com.api.senai;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +11,34 @@ public class SenaiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SenaiApplication.class, args);
+
+		dividePorZero();
+
+		System.out.println("Fim do programa.");
+
 	}
 
+	public static void dividePorZero(){
+
+		System.out.println("Digite um número: ");
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			int numero = sc.nextInt();
+	
+			System.out.println("Informe o divisor: ");
+			// Dispara um erro InputMismatch caso digite uma letra
+			int divisor = sc.nextInt();
+
+			System.out.println("O resultado da divisão é: " 
+
+			// Dispara um erro caso o divisor informado seja 0
+			+ (numero / divisor));
+			
+		} catch (ArithmeticException e) {
+			System.out.println("Não é possível dividir por zero.");
+		} catch (InputMismatchException e) {
+			System.out.println("Divisor informado inválido.");
+		}
+	}
 }
