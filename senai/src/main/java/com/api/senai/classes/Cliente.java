34ade0a257814f752537072c76f3e564118a57ef
Arthur Giangiarulo <111184548+arthurGiangiarulo@users.noreply.github.com>
2024-07-23 20:19:27 -0300

    CRUDs básicos

--- senai/src/main/java/com/api/senai/classes/Cliente.java
+++ senai/src/main/java/com/api/senai/classes/Cliente.java
@@ -1,9 +1,12 @@
 package com.api.senai.classes;
 
+import java.util.ArrayList;
+
 import lombok.Data;
 
 @Data
 public class Cliente {
+    public static ArrayList<Cliente> clientes = new ArrayList<Cliente>();
 
     private String nome;
     private String cpf;
